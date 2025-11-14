fd24f73ff38d434cbcf0fd121b9e2421db2cdd4e
ajayyadav0555 <ay3224587@gmail.com>
2025-11-14 15:25:17 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+
+
+import React from 'react'
+
+export const Counter = ({ initialState }) => {
+  const [count, setCount] = useState(initialState)
+  const [posts, setPosts] = useState([]);
+const [query,setQuery]=useState("")
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  const handleSignChange = () => {
+    setCount((prev) => prev * -1);
+  };
+  // useEffect(() => {
+  //   fetch
+  //     ("https://jsonplaceholder.typicode.com/posts?_limit=3")
+  //     .then((res) => res.json())
+  //     .then((res) => setPosts(res))
+  //     .catch((err) => console.error(err));
+  // }, []);
+
+
+  useEffect(()=>{
+    const handleEnter=(e)=>{
+      console.log("running keydown")
+      if(e.key==="Enter"){
+        handleClick()
+      }
+    }
+
+    document.addEventListener("keydown",handleEnter)
+    return ()=>document.removeEventListener("keydown",handleEnter)
+  },[query])
+const handleClick=async()=>{
+  try {
+    const data=await fetch("http://localhost:8000/ask",{
+    method:"POST",
+    headers:{
+        "Content-Type": "application/json", 
+    },
+    body:JSON.stringify({question:query})
+  })
+  const result=await data.json()
+ setPosts(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+  return (
+    <div><div>
+      <p>{count}</p>
+    </div>
+      <div className="card">
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}>Restart</button>
+        <button onClick={handleSignChange}>Sign Change</button>
+      </div>
+       <div>
+      <h1>Posts</h1>
+   {!posts ? (
+  <p>Loading...</p>
+) : (
+  <div>
+    <p><strong>Question:</strong> {posts?.question}</p>
+    <p><strong>Answer:</strong> {posts?.answer}</p>
+   
+    <p>Confidence: {posts.confidence}</p>
+  </div>
+)}
+
+    </div>
+    <input onChange={(e)=>setQuery(e.target.value)}/><button onClick={handleClick}>Search</button></div>
+  )
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <Counter initialState={6} />
+    </>
+  )
+}
+
+export default App
---
+++ src/Counter.test.jsx
@@ -0,0 +1,74 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { describe, expect, test } from 'vitest'
+import { Counter } from './App'
+
+
+describe("counter component", () => {
+    test("render with initial test", () => {
+        render(<Counter initialState={6} />)
+        expect(screen.getByText("6")).toBeInTheDocument()
+    })
+
+
+    test("check button is increament when click", () => {
+        render(<Counter initialState={0} />)
+        const button = screen.getByText("Increment");
+        fireEvent.click(button);
+        expect(screen.getByText("1")).toBeInTheDocument()
+    })
+
+
+    test("button decreament the value", () => {
+        render(<Counter initialState={10} />)
+        const button = screen.getByText("Decrement")
+        fireEvent.click(button);
+        expect(screen.getByText("9")).toBeInTheDocument()
+    })
+
+    test("reset fuctiality", () => {
+        render(<Counter initialState={8} />)
+        const button = screen.getByText("Restart")
+        fireEvent.click(button)
+        expect(screen.getByText("0")).toBeInTheDocument();
+    })
+
+
+    test("check signChange", () => {
+        render(<Counter initialState={5} />)
+        const button = screen.getByText("Sign Change")
+        fireEvent.click(button)
+        expect(screen.getByText("-5")).toBeInTheDocument()
+    })
+
+
+    test("check api", async () => {
+        const mockData = [
+            { id: 1, title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit" },
+            { id: 2, title: "qui est esse" },
+            { id: 3, title: "ea molestias quasi exercitationem repellat qui ipsa sit aut" },
+        ];
+        vi.spyOn(globalThis, "fetch").mockResolvedValueOnce({
+            json: () => Promise.resolve(mockData),
+        });
+
+
+        render(<Counter initialState={8} />)
+
+        await waitFor(() => {
+            expect(screen.getByText(/sunt aut facere repellat provident occaecati excepturi optio/)).toBeInTheDocument();
+            expect(screen.getByText("qui est esse")).toBeInTheDocument();
+            expect(screen.getByText("ea molestias quasi exercitationem repellat qui ipsa sit aut")).toBeInTheDocument();
+        })
+    })
+
+    test("show loading whilw calling appi",()=>{
+        vi.spyOn(globalThis,"fetch").mockResolvedValueOnce({
+            json:()=>Promise(resolve=>setTimeout(() => {
+                resolve()
+            }, 1000))
+        })
+
+        render(<Counter initialState={7}/>)
+        expect(screen.getByText(/loading/i)).toBeInTheDocument()
+    })
+})
