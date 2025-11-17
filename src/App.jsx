11dcbbc03747c8dc808d31c314824c8cb0f18695
ajayyadav0555 <ay3224587@gmail.com>
2025-11-17 16:31:36 +0530

    fixed test error commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 
 
 
 
-import React from 'react'
 
 export const Counter = ({ initialState }) => {
   const [count, setCount] = useState(initialState)
